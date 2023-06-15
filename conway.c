340f5b24112f35a8ace26012be1118dc3037f64a
TobiasSeeberger <tobias.seeberger1504@gmail.com>
2023-06-15 09:13:17 +0200

    Jedes int zu char gewechselt; start von clock vor die while Schleife gesetzt; pruefeRegeln und printSpielfeld in das Hauptprogramm integriert; Neue Zeit: 13.5

---
+++ conway.c
@@ -0,0 +1,254 @@
+#include <c64.h>
+#include <conio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+#define P_SCREEN0 ((unsigned char *)0x2000)
+#define P_COLOR0  ((unsigned char *)0x0400)
+
+#define P_SCREEN1 ((unsigned char *)0x6000)
+#define P_COLOR1  ((unsigned char *)0x4400)
+
+//HighRes Mode 300x200px
+#define XMAX 40
+#define YMAX 25
+#define BOXSIZE 3
+#define ROUNDS 1
+
+void findNachbarn(char x,char y,char spielfeld[][YMAX],char nachbarn[][BOXSIZE]);
+void printSpielfeld(char spielfeld [][YMAX]);
+char zaehlLebende(char nachbarn[][BOXSIZE]);
+void pruefeRegeln( char x,char y,char lebende,char temp[][YMAX],char spielfeld[][YMAX]);
+
+static char temp[XMAX][YMAX];
+static char nachbarn[BOXSIZE][BOXSIZE];
+
+//static const char array[XMAX][YMAX] 
+static char spielfeld[XMAX][YMAX]= {
+{0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0},
+{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0},
+{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+{0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+{0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+{0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0},
+{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,1},
+{1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,1,1,0,0,0,0,0,0,1,0},
+{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,1},
+{0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+{0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+{0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+{1,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+{0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+{0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+{0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0},
+{0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0},
+{0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0},
+{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,0,0,0,0,0},
+{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0},
+{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0},
+{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+{0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+{0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+{0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0},
+{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0},
+{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,0,0,0,0,0},
+{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+{0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+{0,0,0,1,0,1,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0},
+};
+
+int main(void)
+{
+  clock_t       t;
+  unsigned long sec;
+  unsigned      sec10;
+  unsigned long fps;
+  unsigned      fps10;
+  unsigned char background;
+  unsigned char text;
+        
+	char x, y, lebende;
+	unsigned char round = 0;
+
+  clrscr();
+	background = bgcolor(COLOR_BLACK);
+	text = textcolor(COLOR_WHITE);
+	for(y = 0; y< YMAX; y++){
+        for(x = 0; x< XMAX; x++){
+            if(spielfeld[x][y] == 1){
+                revers(1);
+            } else{
+                revers(0);
+            }        
+             cputcxy (x, y, 32);
+        }
+    }
+//	signal (int sig, __sigfunc func);
+
+	t = clock();
+	while(round < ROUNDS && !kbhit()){
+		for(y = 0; y< YMAX; y++){
+			for(x = 0; x< XMAX; x++){
+				//gotoxy(0,0);
+				//cprintf("%2d %2d",x , y);
+				findNachbarn(x,y,spielfeld,nachbarn);
+				lebende = zaehlLebende(nachbarn);
+				//gotoxy(x,y);
+				//cprintf("%d",lebende /7 );
+				if(spielfeld[x][y] == 0 ){
+		if(lebende == 3){
+			temp[x][y] = 1;
+//			printf("t3\n\n");
+		}
+	}
+	switch (lebende){
+
+                case 2: temp[x][y] = spielfeld[x][y];
+
+                break;
+
+                case 3: temp[x][y] = 1; 
+                break;
+
+                default: temp[x][y] = 0;
+                }
+			}// for x
+		}// for y
+
+		memcpy(spielfeld,temp,XMAX*YMAX);
+	
+		round++;
+		for(y = 0; y< YMAX; y++){
+        for(x = 0; x< XMAX; x++){
+            if(spielfeld[x][y] == 1){
+                revers(1);
+            } else{
+                revers(0);
+            }        
+             cputcxy (x, y, 32);
+        }
+    }
+	}
+		t = clock() - t;
+	
+  /* Reset screen colors */
+    bgcolor (background);
+    textcolor (text);
+    clrscr ();
+
+    /* Calculate stats */
+    sec   = (t * 10) / CLK_TCK;
+    sec10 = sec % 10;
+    sec  /= 10;
+    fps   = (round * (CLK_TCK * 10)) / t;
+    fps10 = fps % 10;
+    fps  /= 10;
+
+    /* Output stats */
+    gotoxy (0, 0); cprintf ("time  : %lu.%us", sec, sec10);
+    gotoxy (0, 1); cprintf ("frames: %lu", round);
+    gotoxy (0, 2); cprintf ("fps   : %lu.%u", fps, fps10);
+
+    /* Wait for a key, then end */
+    cputsxy (0, 4, "Press any key when done...");
+    (void) cgetc ();
+
+    /* Done */
+    return EXIT_SUCCESS;
+}
+
+
+
+
+char zaehlLebende(char nachbarn[][BOXSIZE]){
+  char lebende = 0;
+  char iy, ix;
+	for(iy= 0; iy < BOXSIZE ; iy++){
+		for(ix = 0; ix < BOXSIZE; ix++){
+			//prüfe dass wir nicht auf unserer eigneen position sind
+			if(ix !=1 ||iy !=1)
+			{
+				lebende += nachbarn[ix][iy];
+			}
+		  /* flag = 3;
+			
+			if(ix != 1){
+			flag += 1;
+			}
+			if((iy != 1)){
+			flag +=2;
+			}
+			if(flag >3){
+				lebende += nachbarn[ix][iy];
+			} */
+		}//for ix
+	}//for iy	
+	return lebende;
+}
+
+
+
+void findNachbarn(char x, char y, char spielfeld[][YMAX], char nachbarn[][BOXSIZE]){
+	//gehe über alle nachbarn
+	unsigned char osx, ix;
+	unsigned char osy, iy; 
+	signed char ofy;
+	signed char ofx;
+	char temp;
+	
+	for(ofy = y-1, iy=0; ofy <= (signed char)y+1; ++ofy , ++iy){
+		for(ofx = x-1,ix = 0; ofx <= (signed char)x+1; ++ofx , ++ix){
+	
+			if( ofy < 0)	{
+				osy = YMAX-1;
+			}
+			else if( ofy > YMAX-1)	{
+					osy = 0;
+				}
+				else {
+					osy = ofy;
+				}
+			
+			
+			if( ofx < 0)	{
+				osx = XMAX-1;
+			} else if( ofx > XMAX-1)	{
+					osx = 0;
+				}
+				else {
+					osx = ofx;
+				}
+		  //temp = spielfeld[osx][osy];
+			nachbarn[ix][iy] = spielfeld[osx][osy];				
+		}//for ofx
+	}//for ofy	
+
+}
+
+
+
+
+void printSpielfeld(char spielfeld [][YMAX]){
+	char x,y;
+	for(y = 0; y< YMAX; y++){
+		for(x = 0; x< XMAX; x++){
+			if(spielfeld[x][y] == 1){
+				revers(1);
+			} else{
+				revers(0);
+			}		
+ 			cputcxy (x, y, 32);
+		}
+	}
+}
